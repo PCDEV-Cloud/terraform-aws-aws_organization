143360437d6edce0bae42e70c9c5bf02afd3db58
Paweł Cholewa <pawel.cholewa@pcdev.pl>
2023-08-31 23:29:49 +0200

    Update ou in ou example

--- examples/organizational_units_in_ou/outputs.tf
+++ examples/organizational_units_in_ou/outputs.tf
@@ -1,27 +1,7 @@
-# output "organizational_unit_ids" {
-#   value = module.organizational_units.ids
-# }
-
-# output "organizational_unit_names" {
-#   value = module.organizational_units.names
-# }
-
-output "level_1" {
-  value = module.organizational_units.level_1
+output "organizational_unit_ids" {
+  value = module.organizational_units.ids
 }
 
-output "level_2" {
-  value = module.organizational_units.level_2
-}
-
-output "level_3" {
-  value = module.organizational_units.level_3
-}
-
-output "level_4" {
-  value = module.organizational_units.level_4
-}
-
-output "level_5" {
-  value = module.organizational_units.level_5
+output "organizational_unit_names" {
+  value = module.organizational_units.names
 }

2544cce5c4422cbb5124e77e77e9f2e1bacdff4a
Paweł Cholewa <pawel.cholewa@pcdev.pl>
2023-08-31 23:27:22 +0200

    Update creating from parent ou

--- modules/organizational_units/locals.tf
+++ modules/organizational_units/locals.tf
@@ -15,27 +15,33 @@ locals {
 
   data_organizational_units = merge(local.data_level_0, local.data_level_1, local.data_level_2, local.data_level_3, local.data_level_4, local.data_level_5)
 
-  parent_lvl = var.parent_id != null ? local.data_organizational_units[var.parent_id].level : 0
-  parent_id  = var.parent_id != null ? local.data_organizational_units[var.parent_id].id : data.aws_organizations_organization.this.roots[0].id
+  parent_id  = var.parent_id == null ? data.aws_organizations_organization.this.roots[0].id : local.data_organizational_units[var.parent_id].id
+  parent_lvl = local.data_organizational_units[local.parent_id].level
 
-  parent = tolist([
-    {
-      name        = local.data_organizational_units[local.parent_id].name
-      parent_path = local.data_organizational_units[local.parent_id].parent_path
-      children    = var.organizational_units
-    }
-  ])
+  organizational_units = [for i in var.organizational_units : { name = i.name, children = try(i.children, []), parent_path = local.data_organizational_units[local.parent_id].path }]
 
-  list_of_level_0 = local.parent_lvl == 0 ? local.parent : []
-  list_of_level_1 = concat(flatten([for i in local.list_of_level_0 : [for j in i.children : { name = j.name, parent_path = "/", children = lookup(j, "children", []) }]]), local.parent_lvl == 1 ? local.parent : [])
-  list_of_level_2 = concat(flatten([for i in local.list_of_level_1 : [for j in i.children : { name = j.name, parent_path = "/${i.name}", children = lookup(j, "children", []) }]]), local.parent_lvl == 2 ? local.parent : [])
-  list_of_level_3 = concat(flatten([for i in local.list_of_level_2 : [for j in i.children : { name = j.name, parent_path = "${i.parent_path}/${i.name}", children = lookup(j, "children", []) }]]), local.parent_lvl == 3 ? local.parent : [])
-  list_of_level_4 = concat(flatten([for i in local.list_of_level_3 : [for j in i.children : { name = j.name, parent_path = "${i.parent_path}/${i.name}", children = lookup(j, "children", []) }]]), local.parent_lvl == 4 ? local.parent : [])
-  list_of_level_5 = concat(flatten([for i in local.list_of_level_4 : [for j in i.children : { name = j.name, parent_path = "${i.parent_path}/${i.name}", children = lookup(j, "children", []) }]]), local.parent_lvl == 5 ? local.parent : [])
+  from_ou_list_of_level_1 = local.parent_lvl == 0 ? local.organizational_units : []
+  from_ou_list_of_level_2 = local.parent_lvl == 1 ? local.organizational_units : []
+  from_ou_list_of_level_3 = local.parent_lvl == 2 ? local.organizational_units : []
+  from_ou_list_of_level_4 = local.parent_lvl == 3 ? local.organizational_units : []
+  from_ou_list_of_level_5 = local.parent_lvl == 4 ? local.organizational_units : []
 
-  level_1 = { for i in local.list_of_level_1 : "/${i.name}" => { name = i.name, parent_id = data.aws_organizations_organization.this.roots[0].id, tags = lookup(i, "tags", {}) } }
-  level_2 = { for i in local.list_of_level_2 : "${i.parent_path}/${i.name}" => { name = i.name, parent_id = local.parent_lvl == 2 ? local.parent_id : aws_organizations_organizational_unit.level_1[i.parent_path].id, tags = lookup(i, "tags", {}) } }
-  level_3 = { for i in local.list_of_level_3 : "${i.parent_path}/${i.name}" => { name = i.name, parent_id = local.parent_lvl == 3 ? local.parent_id : aws_organizations_organizational_unit.level_2[i.parent_path].id, tags = lookup(i, "tags", {}) } }
-  level_4 = { for i in local.list_of_level_4 : "${i.parent_path}/${i.name}" => { name = i.name, parent_id = local.parent_lvl == 4 ? local.parent_id : aws_organizations_organizational_unit.level_3[i.parent_path].id, tags = lookup(i, "tags", {}) } }
-  level_5 = { for i in local.list_of_level_5 : "${i.parent_path}/${i.name}" => { name = i.name, parent_id = local.parent_lvl == 5 ? local.parent_id : aws_organizations_organizational_unit.level_4[i.parent_path].id, tags = lookup(i, "tags", {}) } }
+  from_root_list_of_level_1 = local.parent_lvl == 0 ? [] : flatten([for i in local.list_of_level_0 : [for j in i.children : { name = j.name, children = try(j.children, []), parent_path = "/" }]])
+  from_root_list_of_level_2 = local.parent_lvl == 1 ? [] : flatten([for i in local.list_of_level_1 : [for j in i.children : { name = j.name, children = try(j.children, []), parent_path = "/${i.name}" }]])
+  from_root_list_of_level_3 = local.parent_lvl == 2 ? [] : flatten([for i in local.list_of_level_2 : [for j in i.children : { name = j.name, children = try(j.children, []), parent_path = "${i.parent_path}/${i.name}" }]])
+  from_root_list_of_level_4 = local.parent_lvl == 3 ? [] : flatten([for i in local.list_of_level_3 : [for j in i.children : { name = j.name, children = try(j.children, []), parent_path = "${i.parent_path}/${i.name}" }]])
+  from_root_list_of_level_5 = local.parent_lvl == 4 ? [] : flatten([for i in local.list_of_level_4 : [for j in i.children : { name = j.name, children = try(j.children, []), parent_path = "${i.parent_path}/${i.name}" }]])
+
+  list_of_level_0 = var.parent_id == null ? local.organizational_units : [] # what if provided parent_id is root?
+  list_of_level_1 = concat(local.from_ou_list_of_level_1, local.from_root_list_of_level_1)
+  list_of_level_2 = concat(local.from_ou_list_of_level_2, local.from_root_list_of_level_2)
+  list_of_level_3 = concat(local.from_ou_list_of_level_3, local.from_root_list_of_level_3)
+  list_of_level_4 = concat(local.from_ou_list_of_level_4, local.from_root_list_of_level_4)
+  list_of_level_5 = concat(local.from_ou_list_of_level_5, local.from_root_list_of_level_5)
+
+  level_1 = { for i in local.list_of_level_1 : "/${i.name}" => { name = i.name, parent_id = data.aws_organizations_organization.this.roots[0].id, tags = try(i.tags, {}) } }
+  level_2 = { for i in local.list_of_level_2 : "${i.parent_path}/${i.name}" => { name = i.name, parent_id = local.parent_lvl == 1 ? local.parent_id : aws_organizations_organizational_unit.level_1[i.parent_path].id, tags = try(i.tags, {}) } }
+  level_3 = { for i in local.list_of_level_3 : "${i.parent_path}/${i.name}" => { name = i.name, parent_id = local.parent_lvl == 2 ? local.parent_id : aws_organizations_organizational_unit.level_2[i.parent_path].id, tags = try(i.tags, {}) } }
+  level_4 = { for i in local.list_of_level_4 : "${i.parent_path}/${i.name}" => { name = i.name, parent_id = local.parent_lvl == 3 ? local.parent_id : aws_organizations_organizational_unit.level_3[i.parent_path].id, tags = try(i.tags, {}) } }
+  level_5 = { for i in local.list_of_level_5 : "${i.parent_path}/${i.name}" => { name = i.name, parent_id = local.parent_lvl == 4 ? local.parent_id : aws_organizations_organizational_unit.level_4[i.parent_path].id, tags = try(i.tags, {}) } }
 }
